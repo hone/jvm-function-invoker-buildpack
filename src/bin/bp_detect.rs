7e2b7276f2b3ef68371285730f97ed2d0c543187
Terence Lee <hone02@gmail.com>
2021-05-11 16:45:09 -0700

    add support for project.toml

--- src/bin/bp_detect.rs
+++ src/bin/bp_detect.rs
@@ -10,9 +10,11 @@ fn main() {
 fn detect(ctx: GenericDetectContext) -> anyhow::Result<DetectOutcome> {
     let mut buildplan = BuildPlan::new();
 
-    // We check for a function.toml to be able to distinguish between regular JVM applications and a function.
+    // We check for a function.toml/project.toml to be able to distinguish between regular JVM applications and a function.
     // Just from the application alone, they're indistinguishable by design.
-    let outcome = if ctx.app_dir().join("function.toml").exists() {
+    let outcome = if ctx.app_dir().join("function.toml").exists()
+        || ctx.app_dir().join("project.toml").exists()
+    {
         buildplan.requires.push(Require::new("jdk"));
         buildplan.requires.push(Require::new("jvm-application"));
 
